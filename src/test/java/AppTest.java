0e76b6ad1b11e3872f7a9d064fe449015b935327
OderoOluoch <oderoluoch@gmail.com>
2021-06-05 13:11:38 +0300

    Testing in place for the App, re-done

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
