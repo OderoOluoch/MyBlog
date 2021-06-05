a65c9e6eeb1c5d48d4b04b424c125eed9540df47
OderoOluoch <oderoluoch@gmail.com>
2021-06-05 13:33:05 +0300

    Testing creating of Post instances

---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception{
+        Post post = new Post("Day 1:Into");
+        assertEquals(true,post instanceof Post);
+    }
+}
