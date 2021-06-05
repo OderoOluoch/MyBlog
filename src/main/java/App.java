1d1021862248285c303dcc264bebce21cc2bcb87
OderoOluoch <oderoluoch@gmail.com>
2021-06-05 14:46:43 +0300

    User logic laid out

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,26 @@
+import models.Post;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+    get("/", (request, response) -> {
+        Map<String,Object> model = new HashMap<String,Object>();
+        return new ModelAndView(model,"index.hbs");
+    }, new HandlebarsTemplateEngine());
 
+    get("/posts/new",(request, response) -> {
+       Map<String , Object> model = new HashMap<String, Object>();
+       String content = request.queryParams("content");
+       Post newPost = new Post(content);
+       return new ModelAndView(model,"success.hbs");
+    }, new HandlebarsTemplateEngine());
     }
 }
